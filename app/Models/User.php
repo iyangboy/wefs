d05c56b334052975acc35006d76ec4cc42be2f80
杨万里 <1091231787@qq.com>
2019-09-16 15:36:29 +0800

    编辑个人资料

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
      */
     protected $fillable = [
         'name', 'email', 'password',
+        'introduction',
     ];
 
     /**

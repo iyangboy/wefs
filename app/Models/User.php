a8139625e0ecea7e682f43ef79ec98b76b784cd5
杨万里 <1091231787@qq.com>
2019-09-16 15:46:07 +0800

    上传头像

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
      */
     protected $fillable = [
         'name', 'email', 'password',
-        'introduction',
+        'introduction', 'avatar',
     ];
 
     /**

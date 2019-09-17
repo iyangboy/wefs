d73eb0a12794f9f2296a1a55b7abce0b20130d81
杨万里 <1091231787@qq.com>
2019-09-17 18:44:47 +0800

    replies index

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -2,13 +2,43 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use App\Models\Topic;
 use App\Models\Reply;
 use App\Transformers\ReplyTransformer;
 use App\Http\Requests\Api\ReplyRequest;
+use Illuminate\Http\Request;
 
 class RepliesController extends Controller
 {
+    // 回复列表
+    public function index(Topic $topic, Request $request)
+    {
+        //app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+
+        $replies = $topic->replies()->paginate(20);
+
+        if ($request->include) {
+            $replies->load(explode(',', $request->include));
+        }
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+    }
+
+    // 某个用户的回复列表
+    public function userIndex(User $user, Request $request)
+    {
+        //app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+
+        $replies = $user->replies()->paginate(20);
+
+        if ($request->include) {
+            $replies->load(explode(',', $request->include));
+        }
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+    }
+
     // 添加回复
     public function store(ReplyRequest $request, Topic $topic, Reply $reply)
     {

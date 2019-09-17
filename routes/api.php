fe8b9c7b5cf43a2337653a41eace304be3653098
杨万里 <1091231787@qq.com>
2019-09-17 14:53:13 +0800

    verificationCode store

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 $api = app('Dingo\Api\Routing\Router');
 
+/*
 $api->version('v1', function($api) {
     $api->get('version', function() {
         return response('this is version v1');
@@ -30,4 +31,17 @@ $api->version('v2', function($api) {
     $api->get('version', function() {
         return response('this is version v2');
     });
+});
+*/
+
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api'
+], function($api) {
+    // 版本
+    $api->get('version', function() {
+        return response('this is version v1');
+    });
+    // 短信验证码
+    $api->post('verificationCodes', 'VerificationCodesController@store')
+        ->name('api.verificationCodes.store');
 });

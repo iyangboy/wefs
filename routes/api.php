4fd300dd3fe1bb47f98df0be85b41adb739a2c7e
杨万里 <1091231787@qq.com>
2019-09-17 15:08:36 +0800

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -35,18 +35,21 @@ $api->version('v2', function($api) {
 */
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+    'namespace' => 'App\Http\Controllers\Api',
 ], function($api) {
-    // 版本
-    $api->get('version', function() {
-        return response('this is version v1');
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function($api) {
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
     });
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-    // 用户注册
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
 });
 
 

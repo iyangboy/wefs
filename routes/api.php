e7b5caad6dd32882d860c939b52e9e4c97e377dd
杨万里 <1091231787@qq.com>
2019-09-18 17:17:39 +0800

    weapp update user

--- routes/api.php
+++ routes/api.php
@@ -112,6 +112,9 @@ $api->version('v1', [
             // 编辑登录用户信息
             $api->patch('user', 'UsersController@update')
                 ->name('api.user.update');
+            // 小程序编辑登录用户信息（小程序不能使用patch方式）
+            $api->put('user', 'UsersController@update')
+                ->name('api.user.update');
             // 图片资源
             $api->post('images', 'ImagesController@store')
                 ->name('api.images.store');
